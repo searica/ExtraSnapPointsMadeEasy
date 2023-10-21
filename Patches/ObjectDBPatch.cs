0152863565af1c0e29ac633c532037cca918dbd2
searica <143636061+searica@users.noreply.github.com>
2023-10-21 15:09:20 -0700

    Hook when game starts

---
+++ Patches/ObjectDBPatch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+
+namespace ExtraSnapPointsMadeEasy.Patches
+{
+    // hook right when the game first starts
+    // to avoid touching pieces added by mods
+    [HarmonyPatch(typeof(ObjectDB))]
+    internal class ObjectDBPatch
+    {
+        [HarmonyPostfix]
+        [HarmonyPriority(Priority.Low)]
+        [HarmonyPatch(nameof(ObjectDB.Awake))]
+        public static void Awake()
+        {
+            ExtraSnapPoints.AddExtraSnapPoints();
+        }
+    }
+}

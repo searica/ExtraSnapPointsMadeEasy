0f8c0a9db9f66806964bd53d65d949b73da68d57
searica <143636061+searica@users.noreply.github.com>
2023-10-31 10:49:52 -0700

    GetSourceMod() for public API

--- ExtraSnapPointsMadeEasy.cs
+++ ExtraSnapPointsMadeEasy.cs
@@ -1,4 +1,7 @@
 ﻿using BepInEx;
+using BepInEx.Logging;
+using ExtraSnapPointsMadeEasy.Helpers;
+using ExtraSnapPointsMadeEasy.Utils;
 using HarmonyLib;
 using System.Reflection;
 
@@ -13,21 +16,24 @@ namespace ExtraSnapPointsMadeEasy
         public const string PluginGuid = $"{Author}.Valheim.{PluginName}";
         public const string PluginVersion = "1.1.2";
 
+        internal static ExtraSnapPointsMadeEasy Instance;
+
         private void Awake()
         {
+            Instance = this;
             Log.Init(Logger);
-            PluginConfig.Init(Config);
-            PluginConfig.SetUp();
+            Configs.Config.Init(Config);
+            Configs.Config.SetUp();
 
             Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), harmonyInstanceId: PluginGuid);
 
-            PluginConfig.SetupWatcher();
-            PluginConfig.CheckForConfigManager();
+            Configs.Config.SetupWatcher();
+            Configs.Config.CheckForConfigManager();
         }
 
         private void OnDestroy()
         {
-            PluginConfig.Save();
+            Configs.Config.Save();
         }
 
         /// <summary>
@@ -35,10 +41,36 @@ namespace ExtraSnapPointsMadeEasy
         ///     extra snap points after dynamically adding/removing pieces
         ///     from piece tables.
         /// </summary>
-        /// <param name="msg"></param>
-        public static void ReInitExtraSnapPoints(string msg)
+        public static void ReInitExtraSnapPoints()
         {
-            ExtraSnapPoints.AddExtraSnapPoints(msg, true);
+            var pluginInfo = BepInExUtils.GetSourceModMetadata();
+            var msg = $"{pluginInfo.Name} triggered a re-initialization, adding extra snap points";
+            SnapPointAdder.AddExtraSnapPoints(msg, true);
         }
+    }
+
+    /// <summary>
+    /// Helper class for properly logging from static contexts.
+    /// </summary>
+    internal static class Log
+    {
+        internal static ManualLogSource _logSource;
+
+        internal static void Init(ManualLogSource logSource)
+        {
+            _logSource = logSource;
+        }
+
+        internal static void LogDebug(object data) => _logSource.LogDebug(data);
+
+        internal static void LogError(object data) => _logSource.LogError(data);
+
+        internal static void LogFatal(object data) => _logSource.LogFatal(data);
+
+        internal static void LogInfo(object data) => _logSource.LogInfo(data);
+
+        internal static void LogMessage(object data) => _logSource.LogMessage(data);
+
+        internal static void LogWarning(object data) => _logSource.LogWarning(data);
     }
 }
